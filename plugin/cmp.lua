004e60357cdb2b9918a2f8f4780ccedde3185686
tong <tong@disktree.net>
2022-08-28 00:18:52 +0200

    ...

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -1,33 +1,18 @@
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.shortmess:append "c"
+
+local ok, lspkind = pcall(require, "lspkind")
+if not ok then
+    return
+end
+
+lspkind.init()
+
 local cmp = require'cmp'
-local lspkind = require('lspkind')
 local luasnip = require('luasnip')
 
-cmp.setup({
-    formatting = {
-        format = lspkind.cmp_format({
-          mode = 'symbol', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-          -- The function below will be called before any actual modifications from lspkind
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function (entry, vim_item)
-            return vim_item
-          end
-        })
-    },
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-            --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
+cmp.setup {
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -35,21 +20,37 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-    sources = cmp.config.sources(
-    {
+    sources = cmp.config.sources({
+        { name = "git" },
+        -- { name = "gh_issues" },
+        { name = 'nvim_lua' },
         { name = 'nvim_lsp' },
-        -- { name = 'vsnip' },
         { name = 'path', option = { trailing_slash = false }, },
         { name = 'luasnip' },
-        -- { name = 'ultisnips' },
-        -- { name = 'snippy' },
-        { name = "git" },
-        { name = 'nvim_lua' }
+        { name = "buffer", keyword_length = 5 },
+    }),
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
     },
-    {
-        { name = 'buffer' },
-    })
-})
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = true,
+            -- mode = 'symbol', -- show only symbol annotations
+            maxwidth = 72, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            -- before = function (entry, vim_item)
+            --   return vim_item
+            -- end
+        })
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+}
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
--- plugin/vhx.lua
+++
@@ -1,7 +0,0 @@
---require("vhx").setup {
-    --haxe_bin = "haxe",
-    --server = {
-        --port = 9001,
-        --verbose = true
-    --} 
---}
