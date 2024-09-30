5facde33315ab79f67453dcf3373c97e4aedc8fb
tong <tong@disktree.net>
2024-09-30 14:59:00 +0200

    sync

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    -- codelens = {
+    --   enabled = false,
+    -- },
     inlay_hints = {
       --enabled = false,
+      --exclude = { "vue" }
       --show_parameter_hints = false,
     },
   },
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -7,7 +7,7 @@ return {
       },
     },
     window = {
-      width = 32,
+      width = 36,
     },
   },
 }
