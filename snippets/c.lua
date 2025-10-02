e158f7e57b33a7353e6c29b0843b33207d61b8e5
tong <tong@disktree.net>
2025-10-02 14:59:45 +0200

    add snippets (haxe, hxml) in json format

--- snippets/c.lua
+++
@@ -1,5 +0,0 @@
-local parse = require("luasnip.util.parser").parse_snippet
-
-return {
-  parse("printf", 'printf("$1\\n");'),
-}
--- snippets/cpp.lua
+++
@@ -1,2 +0,0 @@
-local ls = require("luasnip")
-ls.filetype_set("cpp", { "c" })
