e881005a739685009c90e5d97f6f898c933875dc
tong <tong@disktree.net>
2025-10-09 14:39:52 +0200

    update tmux navigation plugin config

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,10 +1,45 @@
 return {
   "alexghergh/nvim-tmux-navigation",
-  lazy = false,
+  lazy = true,
+  keys = {
+    {
+      "<C-h>",
+      function()
+        require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
+      end,
+      desc = "Move left",
+    },
+    {
+      "<C-j>",
+      function()
+        require("nvim-tmux-navigation").NvimTmuxNavigateDown()
+      end,
+      desc = "Move down",
+    },
+    {
+      "<C-k>",
+      function()
+        require("nvim-tmux-navigation").NvimTmuxNavigateUp()
+      end,
+      desc = "Move up",
+    },
+    {
+      "<C-l>",
+      function()
+        require("nvim-tmux-navigation").NvimTmuxNavigateRight()
+      end,
+      desc = "Move right",
+    },
+    {
+      "<C-Space>",
+      function()
+        require("nvim-tmux-navigation").NvimTmuxNavigateNext()
+      end,
+      desc = "Next pane",
+    },
+    -- vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+  },
   config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
-    nvim_tmux_nav.setup({
-      disable_when_zoomed = true,
-    })
+    require("nvim-tmux-navigation").setup({ disable_when_zoomed = true })
   end,
 }
